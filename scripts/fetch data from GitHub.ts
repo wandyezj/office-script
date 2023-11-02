fc8c04ba2757a9be6cd6c0448bbf3fe47a3337f9
jowandy <wandyezj@gmail.com>
2023-11-02 09:43:54 -0700

    Update fetch data from GitHub.ts

--- scripts/fetch data from GitHub.ts
+++ scripts/fetch data from GitHub.ts
@@ -1,5 +1,11 @@
 /**
- * Script to show how to fetch public data from GitHub and insert it into a worksheet.
+ * Script to show how to fetch public text file data from GitHub and insert it into a worksheet.
+ * 
+ * How to get the URL on GitHub
+ * 1. On Github click the file in the repository
+ * 2. click raw
+ * 3. Copy the whole url in the address bar
+ *   - For private repositories make sure to include the token in the URL: ?token=...
  */
 async function main(workbook: ExcelScript.Workbook) {
 
@@ -8,8 +14,8 @@ async function main(workbook: ExcelScript.Workbook) {
     const request = await fetch("https://raw.githubusercontent.com/wandyezj/data/master/lists/shapes.list.txt")
     const text = await request.text();
     console.log(text);
-  
+
     // Insert into new worksheet
     const rows = text.split("\n").map(x => [x])
     workbook.addWorksheet().getRange(`A1:A${rows.length}`).setValues(rows);
-  }
+}
